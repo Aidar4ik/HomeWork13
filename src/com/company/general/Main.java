0e5abb4b5de69e21991c1c4d8fefd49c4d3cba3d
Aidar4ik <flayjk@mail.ru>
2020-11-04 20:59:23 +0600

    Initial commit

---
+++ src/com/company/general/Main.java
@@ -0,0 +1,10 @@
+package com.company.general;
+
+import com.company.players.SuperPower;
+import com.company.players.Warrior;
+public class Main {
+
+    public static void main(String[] args) {
+        RPG_Game.start();
+    }
+}
---
+++ src/com/company/players/SuperPower.java
@@ -0,0 +1,5 @@
+package com.company.players;
+
+public enum SuperPower {
+    HEAL, SAVE_DAMAGE_AND_REVERT, CRITICAL_DAMAGE, BOOST,RESURRECTION, STUN, DOUBLE_ARROW,BLOCK,
+}
